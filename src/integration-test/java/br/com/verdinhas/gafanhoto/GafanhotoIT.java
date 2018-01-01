9044b59b61fd21a3e425ee9b26e28dc2c11d10aa
robsonbittencourt <robson.luizv@gmail.com>
2018-01-01 17:55:10 -0200

    Separate integration tests

--- src/integration-test/java/br/com/verdinhas/gafanhoto/GafanhotoIT.java
+++ src/integration-test/java/br/com/verdinhas/gafanhoto/GafanhotoIT.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GafanhotoTest {
+public class GafanhotoIT {
 
 	@Autowired
 	private Gafanhoto gafanhoto;
