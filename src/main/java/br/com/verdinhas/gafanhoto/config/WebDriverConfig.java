c1b5616b148414701d7148dd1092ea29608c2477
robsonbittencourt <robson.luizv@gmail.com>
2017-12-31 22:39:43 -0200

    Fix tests to run correctly in the Travis CI

--- src/main/java/br/com/verdinhas/gafanhoto/config/WebDriverConfig.java
+++ src/main/java/br/com/verdinhas/gafanhoto/config/WebDriverConfig.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class WebDriverConfig {
 
 	@Bean
 	@Lazy
+	@Scope("prototype")
 	public WebDriver webDriver() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
+		options.addArguments("always-authorize-plugins");
+		options.addArguments("dns-prefetch-disable");
 
 		return new ChromeDriver(options);
 	}
