e84337bb7fa5f2d18d38869486a213a05a3469c9
robsonbittencourt <robson.luizv@gmail.com>
2018-01-19 23:24:15 -0200

    Improve delete command

--- src/main/java/br/com/verdinhas/gafanhoto/monitor/MonitorValidator.java
+++ src/main/java/br/com/verdinhas/gafanhoto/monitor/MonitorValidator.java
@@ -3,10 +3,14 @@ package br.com.verdinhas.gafanhoto.monitor;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import br.com.verdinhas.gafanhoto.telegram.GafanhotoBot;
 import br.com.verdinhas.gafanhoto.telegram.ReceivedMessage;
@@ -23,8 +27,24 @@ public class MonitorValidator {
 		List<Monitor> userMonitors = monitorRepository.findByUserId(message.userId());
 
 		if (isNotEmpty(userMonitors) && userMonitors.size() == MAX_MONITORS_BY_USER) {
-			bot.sendMessage(message.chatId(),
+			SendMessage sendMessage = new SendMessage().setChatId(message.chatId()).setText(
 					"Você quer boletar muitas coisas hein. Já existem 10 monitores cadastrados. Apague algum para cadastrar um novo.");
+
+			InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+			List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+			List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+			InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton().setText("Apagar monitores")
+					.setCallbackData("/apagar");
+			rowInline.add(inlineKeyboardButton);
+
+			rowsInline.add(rowInline);
+
+			markupInline.setKeyboard(rowsInline);
+			sendMessage.setReplyMarkup(markupInline);
+
+			bot.execute(sendMessage);
+
 			return true;
 		}
 
